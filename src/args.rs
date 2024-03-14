82e1aa8a5f5e2cfedf6ec81a93abbae48da81a43
cndofx <19335833+cndofx@users.noreply.github.com>
2024-03-13 22:50:45 -0600

    impl init command

--- src/args.rs
+++ src/args.rs
@@ -3,19 +3,19 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 pub struct Args {
     #[command(subcommand)]
-    command: Command,
+    pub command: Command,
 }
 
 #[derive(Subcommand)]
 pub enum Command {
     /// Should only be called by Steam with "protonhax init %command%"
-    Init,
-    /// Lists all currently running games
-    Ls,
+    Init { command: Vec<String> },
     /// Runs a command in the context of the given appid with proton
     Run { appid: u16, command: String },
     /// Runs a command in the context of the given appid
     Exec { appid: u16, command: String },
     /// Runs cmd.exe in the context of the given appid
     Cmd { appid: u16 },
+    /// Lists all currently running games
+    Ls,
 }
